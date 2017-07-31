59035f2e4c303add8585a630f8a5c6dda5e87c8c
Anjali <official.anjali@gmail.com>
2017-07-31 15:30:21 +0530

    Update jenkinstest.java

--- jenkinstest.java
+++ jenkinstest.java
@@ -2,7 +2,7 @@ public class jenkinstest {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
       int i = 0;
-      for (i= 0 ; i<= 1000000000; i++)
+      for (i= 0 ; i<= 10000000; i++)
       {         
       System.out.println("Hello, World");
       }
